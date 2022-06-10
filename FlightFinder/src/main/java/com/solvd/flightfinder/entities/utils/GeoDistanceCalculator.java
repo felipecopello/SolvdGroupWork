63862e0e0feae50ab3c4c4aafb3d5358612f2f32
kdenysiuk <103523831+kdenysiuk@users.noreply.github.com>
2022-06-10 09:45:33 -0300

    Distance Caluclator Util added (#2)
    * util added
    
    Co-authored-by: ¨kdenysiuk¨ <¨kdenysiuk@solvd.com¨>

---
+++ FlightFinder/src/main/java/com/solvd/flightfinder/entities/utils/GeoDistanceCalculator.java
@@ -0,0 +1,20 @@
+package com.solvd.flightfinder.entities.utils;
+
+public class GeoDistanceCalculator {
+
+    public static double distance(double lat1, double lon1, double lat2, double lon2) {
+        if ((lat1 == lat2) && (lon1 == lon2)) {
+            return 0;
+        }
+        else {
+            double theta = lon1 - lon2;
+            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+            dist = Math.acos(dist);
+            dist = Math.toDegrees(dist);
+            dist = dist * 60 * 1.1515;
+            dist = dist * 1.609344;
+            return (dist);
+        }
+    }
+
+}
