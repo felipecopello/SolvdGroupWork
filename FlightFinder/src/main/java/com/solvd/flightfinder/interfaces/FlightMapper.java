a1102ba4d68693567bd809464c35d07ea2786338
felipecopello <90850725+felipecopello@users.noreply.github.com>
2022-06-15 14:04:02 +0200

    Felipe (#9)
    * added db.properties
    
    * finished all mappers.xml and interfaces, to support CRUD operations
    
    * fixed minor issued with mybatis flight mapper
    
    * finished mybatis factory
    
    * work on flight filter

--- FlightFinder/src/main/java/com/solvd/flightfinder/interfaces/FlightMapper.java
+++ FlightFinder/src/main/java/com/solvd/flightfinder/interfaces/FlightMapper.java
@@ -1,7 +1,12 @@
 package com.solvd.flightfinder.interfaces;
 
+import java.util.List;
+
 import com.solvd.flightfinder.entities.Flight;
 
 public interface FlightMapper extends IEntityMapper<Flight> {
 
+	public List<Flight> getByDepartureAirportId(long departureAirportId);
+
+	public List<Flight> getByArrivalAirportId(long arrivalAirportId);
 }
---
+++ FlightFinder/src/main/java/com/solvd/flightfinder/utils/MyBatisFactory.java
@@ -0,0 +1,57 @@
+package com.solvd.flightfinder.utils;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.solvd.flightfinder.interfaces.AirlineMapper;
+import com.solvd.flightfinder.interfaces.AirportMapper;
+import com.solvd.flightfinder.interfaces.CityMapper;
+import com.solvd.flightfinder.interfaces.CountryMapper;
+import com.solvd.flightfinder.interfaces.FlightMapper;
+import com.solvd.flightfinder.interfaces.IEntityMapper;
+import com.solvd.flightfinder.interfaces.PassengerMapper;
+import com.solvd.flightfinder.interfaces.TicketMapper;
+
+import util.SqlSessionFactoryUtil;
+
+public class MyBatisFactory {
+
+	private static SqlSessionFactory factory = SqlSessionFactoryUtil.getInstance();
+	private static SqlSession session = factory.openSession();
+
+	public static IEntityMapper<?> getMyBatis(String myBatis) {
+		myBatis = myBatis.toLowerCase();
+		switch (myBatis) {
+		case "airline":
+			AirlineMapper airlineMapper = session.getMapper(AirlineMapper.class);
+			return airlineMapper;
+		case "airport":
+			AirportMapper airportMapper = session.getMapper(AirportMapper.class);
+			return airportMapper;
+		case "city":
+			CityMapper cityMapper = session.getMapper(CityMapper.class);
+			return cityMapper;
+		case "country":
+			CountryMapper countryMapper = session.getMapper(CountryMapper.class);
+			return countryMapper;
+		case "flight":
+			FlightMapper flightMapper = session.getMapper(FlightMapper.class);
+			return flightMapper;
+		case "passenger":
+			PassengerMapper passengerMapper = session.getMapper(PassengerMapper.class);
+			return passengerMapper;
+		case "ticket":
+			TicketMapper ticketMapper = session.getMapper(TicketMapper.class);
+			return ticketMapper;
+		}
+		return null;
+	}
+
+	public static SqlSession getSession() {
+		return session;
+	}
+
+	public void setSession(SqlSession session) {
+		MyBatisFactory.session = session;
+	}
+}
