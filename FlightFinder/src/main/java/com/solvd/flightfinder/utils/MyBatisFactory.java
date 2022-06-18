ae380549e7a8f04ec0f71579b46b8659dc663eda
felipe <felipecopello94@gmail.com>
2022-06-18 22:47:16 +0200

    update

--- FlightFinder/src/main/java/com/solvd/flightfinder/utils/MyBatisFactory.java
+++ FlightFinder/src/main/java/com/solvd/flightfinder/utils/MyBatisFactory.java
@@ -12,8 +12,6 @@ import com.solvd.flightfinder.interfaces.IEntityMapper;
 import com.solvd.flightfinder.interfaces.PassengerMapper;
 import com.solvd.flightfinder.interfaces.TicketMapper;
 
-import util.SqlSessionFactoryUtil;
-
 public class MyBatisFactory {
 
 	private static SqlSessionFactory factory = SqlSessionFactoryUtil.getInstance();
